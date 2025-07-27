d9bf1faf2af07d0cff2d7bb815a3fb9619cbf0a6
André Ricardo <contatoadmx@gmail.com>
2025-07-27 12:08:38 -0300

    deploy not install

--- system/terminal/deploy.php
+++ system/terminal/deploy.php
@@ -6,8 +6,6 @@ return new class extends Terminal {
 
     function __invoke()
     {
-        Terminal::run('install');
-
         self::echo('Command [deploy] running');
 
         Terminal::run('composer');
