1c249ffafd242f2def778ed144acf5795366365b
André Ricardo <contatoadmx@gmail.com>
2025-07-27 10:54:25 -0300

    install php mx

--- system/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use PhpMx\Router;
-
-Router::group('api', [], function () {
-    Router::get('...', STS_NOT_FOUND);
-    Router::get('', STS_OK);
-});
--- system/routes/front.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use PhpMx\Router;
-
-Router::middleware(['front'], function () {
-    Router::get('...', STS_NOT_FOUND);
-    Router::get('', STS_OK);
-});
